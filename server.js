beb20fec82a679b03cc39b7bd1369d3706c67410
Caio Melzer <melzer.caio@gmail.com>
2022-05-05 09:38:47 -0300

    Change Port

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.use(pathApi+'/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+//const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 4001) : 4000;
 
 app.listen(port, () => console.log('Server listening on port ' + port));
 
